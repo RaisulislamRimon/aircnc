ee74a14e984a80d9f5e84c5c5d0d26991fee55da
RaisulislamRimon <rimonppip0@gmail.com>
2022-12-17 22:57:26 +0600

    project setup

--- aircnc-client/src/main.jsx
+++ aircnc-client/src/main.jsx
@@ -1,10 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
+import { Toaster } from "react-hot-toast";
+import "react-datepicker/dist/react-datepicker.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
+    <Toaster />
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
