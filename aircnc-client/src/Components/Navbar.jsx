1076fa135a8b890b8b2ae3c4ed2600356e4cf7da
RaisulislamRimon <rimonppip0@gmail.com>
2022-12-25 22:56:40 +0600

    added logout in navbar

--- aircnc-client/src/Components/Navbar.jsx
+++ aircnc-client/src/Components/Navbar.jsx
@@ -5,7 +5,18 @@ import { AuthContext } from "../contexts/AuthProvider";
 import PrimaryButton from "./Button/PrimaryButton";
 
 const Navbar = () => {
-  const { user } = useContext(AuthContext);
+  const {
+    user,
+    createUser,
+    updateUserProfile,
+    verifyEmail,
+    signInWithGoogle,
+    logout,
+    signin,
+    resetPassword,
+    loading,
+    setLoading,
+  } = useContext(AuthContext);
   const [isDropdownOpen, setIsDropdownOpen] = useState(false);
 
   return (
@@ -61,7 +72,10 @@ const Navbar = () => {
                     </Link>
 
                     <hr className="border-gray-200" />
-                    <div className="flex items-center cursor-pointer p-3 text-sm text-gray-600 capitalize transition-colors duration-200 transform  hover:bg-gray-100 ">
+                    <div
+                      onClick={logout}
+                      className="flex items-center cursor-pointer p-3 text-sm text-gray-600 capitalize transition-colors duration-200 transform  hover:bg-gray-100 "
+                    >
                       <svg
                         className="w-5 h-5 mx-1"
                         viewBox="0 0 24 24"
